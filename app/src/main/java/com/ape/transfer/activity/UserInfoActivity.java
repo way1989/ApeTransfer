dc5e4bc8b7c4b5104b68f9f2a99156dc91ba0ee7
way <1320438999@qq.com>
2016-06-12 00:06:20 +0800

    add user info

---
+++ app/src/main/java/com/ape/transfer/activity/UserInfoActivity.java
@@ -0,0 +1,15 @@
+package com.ape.transfer.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ape.transfer.R;
+
+public class UserInfoActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}
